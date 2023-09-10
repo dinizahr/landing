08e3975ec609a682473d4ddba723fd15da8503d2
dinizahr <108143463+dinizahr@users.noreply.github.com>
2023-09-10 12:25:12 +0700

    tugas pmwb
    tugas mapel pmwb

---
+++ landing/index.php
@@ -0,0 +1,22 @@
+<?
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>My_Website</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div class="main">
+            <div class="navbar">
+                <label class="logo">My_Website</label>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="https://rgalauku.blogspot.com/">Blogspot</a></li>
+                    <li><a href="aboute.html">Aboute</a></li>
+                    <li><a href="tim.html">workmate</a></li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
